46f85043865ecd7a135185428ec5d1f91562f8d6
Ankit <157349284+ankit-80@users.noreply.github.com>
2025-12-30 14:23:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@
     bucket = "airbnb-proj-dev-bucket-123"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "airbnb_proj-dev-terraform-lock-db-table" # used created db table
+    dynamodb_table = "terraform-lock-db-table" # used created db table
   }
 }
