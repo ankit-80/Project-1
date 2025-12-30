354077e5691c82716b5462574c03488b74e2ae81
Ankit <157349284+ankit-80@users.noreply.github.com>
2025-12-30 10:11:29 +0530

    Update s3_bucket_creation.tf

--- Jenkins_server_creation/s3_bucket_creation.tf
+++ Jenkins_server_creation/s3_bucket_creation.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket =  var.bucket_name
 
   tags = {
-    Name        = "${var.env_prefix}-bucket"
+    Name        = "${var.env_prefix}-bucket-123"
     Environment = var.env_prefix
   }
 }
@@ -12,4 +12,5 @@ resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
   versioning_configuration {
     status = "Enabled"
   }
+
 }
